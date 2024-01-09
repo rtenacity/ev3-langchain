d56f5012b043e470f038d9f582f05179f07f9a18
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2024-01-09 13:22:48 -0500

    bot

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from openai import OpenAI
 import os
-from dotenv import load_dotenv
 import http.client
 import json
+from robot import Robot
 
-load_dotenv()
-
+bot = Robot()
 app = Flask(__name__)
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
