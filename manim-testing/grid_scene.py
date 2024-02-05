47f45fd42bdd70d048efc337a568c77f52b39dea
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2024-02-05 14:12:51 -0500

    refactoring

--- manim-testing/grid_scene.py
+++ manim-testing/grid_scene.py
@@ -7,24 +7,16 @@ config.frame_width = 16.0
 config.background_color = WHITE
 
 class Bot:
-    def __init__(self, scene):
+    def __init__(self, scene, color, initial_position):
         self.scene = scene
         grid_space_scale = 0.2
-        self.blue_box = Square(color=BLUE).scale(3*grid_space_scale)
-        self.red_box = Square(color=RED).scale(3*grid_space_scale)
-        self.blue_box.move_to(self._grid_to_scene_coords((12.5, 25)))
-        self.red_box.move_to(self._grid_to_scene_coords((37.5, 25)))
+        self.box = Square(color=color).scale(3*grid_space_scale)
+        self.box.move_to(self._grid_to_scene_coords(initial_position))
     
-    def move_to_point(self, box, point, run_time=2):
+    def move_to_point(self, point, run_time=2):
         target_position = self._grid_to_scene_coords(point)
-        path = Line(box.get_center(), target_position, color=box.color)
-        self.scene.play(MoveAlongPath(box, path), run_time=run_time)
-    
-    def move_blue_box(self, point, run_time=2):
-        self.move_to_point(self.blue_box, point, run_time)
-    
-    def move_red_box(self, point, run_time=2):
-        self.move_to_point(self.red_box, point, run_time)
+        path = Line(self.box.get_center(), target_position, color=self.box.color)
+        self.scene.play(MoveAlongPath(self.box, path), run_time=run_time)
     
     def _grid_to_scene_coords(self, point):
         x, y = point
@@ -34,7 +26,6 @@ class Bot:
 
 class MyScene(Scene):
     def construct(self):
-        self.bot = Bot(self)
         grid = NumberPlane(
             x_range=[0, 50, 5],
             y_range=[0, 50, 5],
@@ -49,14 +40,19 @@ class MyScene(Scene):
         self.add(grid)
         line = Line(start=grid.c2p(25, 0), end=grid.c2p(25, 50), color=BLACK, stroke_width=2)
         self.add(line)
-        self.add(self.bot.blue_box, self.bot.red_box)
+        
+        # Create instances of Bot for blue and red boxes
+        self.blue_box_bot = Bot(self, BLUE, (12.5, 25))
+        self.red_box_bot = Bot(self, RED, (37.5, 25))
+        self.add(self.blue_box_bot.box, self.red_box_bot.box)
 
 class TestScene(MyScene):
     def construct(self):
         super().construct()
-        self.bot.move_blue_box((12.5, 25))
+        self.blue_box_bot.move_to_point((12.5, 25))
         self.wait(1)
-        self.bot.move_blue_box((12.5, 50))
+        self.blue_box_bot.move_to_point((12.5, 50))
         self.wait(1)
-        self.bot.move_red_box((25, 25))
+        self.red_box_bot.move_to_point((25, 25))
         self.wait(1)
+
